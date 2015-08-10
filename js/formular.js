dcc540790b75bc22188069da37c8244dc0f3fe9e
mark-mit-k <warneke.mark@gmail.com>
2015-08-10 23:47:32 +0200

    Anpassungen fuer Termin

--- js/formular.js
+++ js/formular.js
@@ -26,31 +26,31 @@ $(document).ready(function() {
   if($.support.placeholder) {
     $('.form-group').each(function(){
       $(this).addClass('js-hide-label');
-    });  
+    });
   }
 
   $('.form-group').find('input, textarea').on('keyup blur focus', function(e){
-     
+
     // Cache our selectors
     var $this = $(this),
         $parent = $this.parent();
- 
+
     // Add or remove classes
     if (e.type == 'keyup') {
       if( $this.val() == '' ) {
-          $parent.addClass('js-hide-label'); 
+          $parent.addClass('js-hide-label');
       } else {
-          $parent.removeClass('js-hide-label');   
+          $parent.removeClass('js-hide-label');
       }
-    } 
+    }
     else if (e.type == 'blur') {
       if( $this.val() == '' ) {
         $parent.addClass('js-hide-label');
-      } 
+      }
       else {
         $parent.removeClass('js-hide-label').addClass('js-unhighlight-label');
       }
-    } 
+    }
     else if (e.type == 'focus') {
       if( $this.val() !== '' ) {
         $parent.removeClass('js-unhighlight-label');
@@ -64,7 +64,7 @@ $(document).ready(function() {
 
     $(ref).each(function(){
         if ( $(this).val() == '' )
-        {   
+        {
             var prevValue = $(this).val();
             $(this).addClass('js-validation-input');
             e.preventDefault();
@@ -78,12 +78,12 @@ $(document).ready(function() {
   });
 
 });
-     
-//Erzeugt die Bildvorschau   
+
+//Erzeugt die Bildvorschau
 function previewImage(e)
 {
     var img = $(document.createElement('img'));
     $('#preview img').remove();
-    $(img).attr('src', e.target.result).width('450px').height('600px');
+    $(img).attr('src', e.target.result).height('400px');
     $('#preview').append(img);
 }
